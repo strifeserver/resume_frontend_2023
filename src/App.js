4683a3d86bb7cbedb8f8af5befd6293d85371468
tcapjean <133844623+tcapjean@users.noreply.github.com>
2023-10-11 03:48:17 +0800

    Tailwind added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import 'output.css'; // Import the CSS file using the symlink
 
-import ProjectList from "./components/ProjectList";
-import CreateProject from "./components/CreateProject";
-import EditProject from "./components/EditProject";
-
+import Navigation from "./components/Navigation";
+// import './dist/output.css'; // Import the CSS file
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">HOME</Link>
-            </li>
-            <li>
-              <Link to="/">PROJECTS</Link>
-            </li>
-            <li>
-              <Link to="/">ABOUT</Link>
-            </li>
-            <li>
-              <Link to="/">CONTACT</Link>
-            </li>
-            <li>
-              <Link to="/create">Create Project</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/create" element={<CreateProject />} />
-          <Route path="/" element={<ProjectList />} />
-          <Route path="/edit-project/:id" element={<EditProject />} />
-          {/* <Route path="/edit-project/:id" element={EditProject} /> */}
-        </Routes>
-      </div>
-    </Router>
+    <Navigation/>
   );
 }
 
--- src/components/CreateProject.js
+++ src/components/CreateProject.js
@@ -4,35 +4,9 @@ import React from "react";
 import ProjectForm from "../components/reusables/ProjectForm";
 
 function CreateProject() {
-  // const [projectName, setProjectName] = useState("");
-  // const [projectDescription, setProjectDescription] = useState("");
-  // const [languages, setLanguages] = useState("");
-  // const [orderBy, setOrderBy] = useState(0);
-  // const [company, setCompany] = useState("");
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-
-  //   // // Create a new project
-  //   // axios.post("http://localhost:5000/api/projects", {
-  //   //   project_name: projectName,
-  //   //   project_description: projectDescription,
-  //   //   languages: languages,
-  //   //   order_by: orderBy,
-  //   //   company: company,
-  //   // })
-  //   //   .then((response) => {
-  //   //     console.log("Project created:", response.data);
-  //   //     // Optionally, you can redirect or update the project list here.
-  //   //   })
-  //   //   .catch((error) => {
-  //   //     console.error("Error creating project:", error);
-  //   //   });
-  // };
-
   return (
     <div>
-      <h2>Create Project</h2>
+      <h2>Add Project</h2>
       <ProjectForm mode="create" />
 
     </div>
